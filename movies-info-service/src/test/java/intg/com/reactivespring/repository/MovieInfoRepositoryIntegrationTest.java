0e70022d2890bb52579965ef4372bf73f7672c90
John Eatwell <engineerarchitect@users.noreply.github.com>
2026-05-31 18:33:03 +0200

    added test cases using testcontainers
    These appears to be way easier to integrate than the embeded method. One
    interesting integration would be to use Podman containers

---
+++ movies-info-service/src/test/java/intg/com/reactivespring/repository/MovieInfoRepositoryIntegrationTest.java
@@ -0,0 +1,64 @@
+package com.reactivespring.repository;
+
+import com.reactivespring.domain.MovieInfo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@DataMongoTest
+@Testcontainers
+@ActiveProfiles("test")
+class MovieInfoRepositoryIntegrationTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0")
+            .withExposedPorts(27017);
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Autowired
+    MovieInfoRepository movieInfoRepository;
+
+    @BeforeEach
+    void setUp() {
+        var movieinfos = List.of(
+                new MovieInfo(null, "Batman Begins", 2005,
+                        List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight", 2008,
+                        List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises", 2012,
+                        List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"))
+        );
+
+        movieInfoRepository.saveAll(movieinfos)
+                .blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        movieInfoRepository.deleteAll().block();
+    }
+
+    @Test
+    void findAll() {
+        var moviesInfoFlux = movieInfoRepository.findAll();
+        StepVerifier.create(moviesInfoFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+}

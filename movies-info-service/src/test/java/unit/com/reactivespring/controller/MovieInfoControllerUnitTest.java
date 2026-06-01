cccb344824038a399b5e1a9b80a0095f6fe2941c
John Eatwell <engineerarchitect@users.noreply.github.com>
2026-06-01 20:48:41 +0200

    added exception handling and validation
    Note the use of NotBlank in MoviInfo for use in Colliection validation

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/MovieInfoControllerUnitTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/MovieInfoControllerUnitTest.java
@@ -100,6 +100,39 @@ public class MovieInfoControllerUnitTest {
     }
 
     @Test
+    void addNewMovieInfo_validation() {
+
+        var movieInfo = new MovieInfo(null, "",
+                -2005, List.of(""), LocalDate.parse("2005-06-15"));
+
+        webTestClient
+                .post()
+                .uri(MOVIES_INFO_URL)
+                .bodyValue(movieInfo)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(String.class)
+                .consumeWith(entityExchangeResult -> {
+                    var responseBody = entityExchangeResult.getResponseBody();
+                    System.out.println("responseBody : " + responseBody);
+                    var expectedErrorMessage = "movieInfo.cast must be present,movieInfo.name must be present,movieInfo.year must be a Positive Value";
+                   Assertions.assertEquals(expectedErrorMessage, responseBody);
+                });
+                /*.expectBody()
+                .jsonPath("$.error").isEqualTo("Bad Request");*/
+
+//                .expectBody(String.class)
+//                .consumeWith(result -> {
+//                    var error = result.getResponseBody();
+//                    assert  error!=null;
+//                    String expectedErrorMessage = "movieInfo.cast must be present,movieInfo.name must be present,movieInfo.year must be a Positive Value";
+//                    assertEquals(expectedErrorMessage, error);
+//
+//                });
+    }
+
+    @Test
     void updateMovieInfo() {
         var id = "abc";
         var updatedMovieInfo = new MovieInfo("abc", "Dark Knight Rises 1",

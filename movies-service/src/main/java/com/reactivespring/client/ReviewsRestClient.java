52418c3a0ce6c17aacf953c78ff3bc7e1fa25f3a
John Eatwell <engineerarchitect@users.noreply.github.com>
2026-06-21 13:56:49 +0200

    implement Client error exception handling in MoviewInfo rest Call

--- movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
@@ -1,12 +1,19 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.Review;
+import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.ReviewsClientException;
+import com.reactivespring.exception.ReviewsServerException;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
